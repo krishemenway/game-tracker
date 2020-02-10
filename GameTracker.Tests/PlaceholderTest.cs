d27f58f1083ef21a1ed7e484bd22c6d7919b481a
Kris Hemenway <krishemenway@gmail.com>
2020-02-10 01:28:11 -0800

    added placeholder test project

---
+++ GameTracker.Tests/PlaceholderTest.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace GameProfile.Service.Tests
+{
+	[TestFixture]
+	public class PlaceholderTest
+	{
+		[Test]
+		public void Test()
+		{
+			true.Should().BeTrue();
+		}
+	}
+}
