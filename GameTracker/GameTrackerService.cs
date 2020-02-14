429fef9ea21b22927b777d3f47405564831209c5
Kris Hemenway <krishemenway@gmail.com>
2020-02-13 21:11:11 -0800

    moving logging of various file paths into own method to improve code readability

--- GameTracker/GameTrackerService.cs
+++ GameTracker/GameTrackerService.cs
@@ -15,10 +15,8 @@ namespace GameTracker
 
 		public bool Start()
 		{
+			LogUsefulInformation();
 			Timer.Start();
-			Log.Information("Writing ProcessSessions to {ProcessSessionsPath}", ProcessSessionStore.DataFilePath);
-			Log.Information("Writing ObservedProcesses to {ObservedProcessesPath}", ObservedRunningProcessStore.DataFilePath);
-
 			return true;
 		}
 
@@ -26,6 +24,12 @@ namespace GameTracker
 		{
 			Timer.Stop();
 			return true;
+		}
+
+		private void LogUsefulInformation()
+		{
+			Log.Information("Writing ProcessSessions to {ProcessSessionsPath}", ProcessSessionStore.DataFilePath);
+			Log.Information("Writing ObservedProcesses to {ObservedProcessesPath}", ObservedRunningProcessStore.DataFilePath);
 		}
 
 		private Timer Timer { get; set; }
