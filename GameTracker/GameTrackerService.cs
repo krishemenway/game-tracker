845c25ba17aaaa02b25176f4224c57e523df2e3b
Kris Hemenway <krishemenway@gmail.com>
2020-02-14 22:53:15 -0800

    added webapi support and controller to list out observed running processes

--- GameTracker/GameTrackerService.cs
+++ GameTracker/GameTrackerService.cs
@@ -1,22 +1,36 @@
 ﻿using GameTracker.ObservedProcesses;
 using GameTracker.ProcessSessions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Serilog;
 using System.Timers;
 
 namespace GameTracker
 {
-	internal class GameTrackerService
+	public class GameTrackerService
 	{
 		public GameTrackerService()
 		{
-			Timer = new Timer(15000) { AutoReset = true };
+			Timer = new Timer(Program.Configuration.GetValue<int>("ProcessScanIntervalInSeconds") * 1000) { AutoReset = true };
 			Timer.Elapsed += new ElapsedEventHandler((sender, args) => new ProcessScanner().ScanProcesses());
+
+			WebHost = new WebHostBuilder()
+				.UseKestrel()
+				.UseStartup<WebHostConfiguration>()
+				.UseConfiguration(Program.Configuration)
+				.UseSerilog()
+				.UseUrls(WebHostConfiguration.WebHostListenAddress)
+				.Build();
 		}
 
 		public bool Start()
 		{
 			LogUsefulInformation();
 			Timer.Start();
+			WebHost.Start();
 			return true;
 		}
 
@@ -30,8 +44,34 @@ namespace GameTracker
 		{
 			Log.Information("Writing ProcessSessions to {ProcessSessionsPath}", ProcessSessionStore.DataFilePath);
 			Log.Information("Writing ObservedProcesses to {ObservedProcessesPath}", ObservedRunningProcessStore.DataFilePath);
+			Log.Information("Starting web host on {WebHostListenAddress}", WebHostConfiguration.WebHostListenAddress);
 		}
 
 		private Timer Timer { get; set; }
+		private IWebHost WebHost { get; set; }
+	}
+
+	public class WebHostConfiguration
+	{
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvcCore().AddJsonOptions(FixJsonCamelCasing);
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app)
+		{
+			app.UseRouting();
+			app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+		}
+
+		private void FixJsonCamelCasing(JsonOptions options)
+		{
+			// this unsets the default behavior (camelCase); "what you see is what you get" is now default
+			options.JsonSerializerOptions.PropertyNamingPolicy = null;
+		}
+
+		public static string WebHostListenAddress => $"http://*:{Program.Configuration.GetValue<string>("WebPort")}";
 	}
 }
---
+++ GameTracker/ObservedProcesses/FindAllObservedRunningProcessesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GameTracker.ObservedProcesses
+{
+	[Route("webapi")]
+	public class FindAllObservedRunningProcessesController : ControllerBase
+	{
+		/// <summary></summary>
+		/// <returns></returns>
+		[HttpGet(nameof(FindAllObservingProcesses))]
+		public ActionResult<ObservedRunningProcessesResult> FindAllObservingProcesses()
+		{
+			return new ObservedRunningProcessesResult
+				{
+					ObservedProcesses = new ObservedRunningProcessStore().FindAll(),
+				};
+		}
+	}
+}
