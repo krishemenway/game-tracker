bf50683ca8920fa2ed2b4b86e7c46a96badce0ff
Kris Hemenway <krishemenway@gmail.com>
2020-03-02 00:07:44 -0800

    added missing glob json converter

---
+++ GameTracker/GlobJsonConverter.cs
@@ -0,0 +1,20 @@
+﻿using GlobExpressions;
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace GameTracker
+{
+	public class GlobJsonConverter : JsonConverter<Glob>
+	{
+		public override Glob Read(ref Utf8JsonReader reader, Type objectType, JsonSerializerOptions options)
+		{
+			return new Glob(reader.GetString());
+		}
+
+		public override void Write(Utf8JsonWriter writer, Glob value, JsonSerializerOptions options)
+		{
+			writer.WriteStringValue(value.Pattern);
+		}
+	}
+}
