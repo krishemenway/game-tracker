76e23d4b4fd0448d5f8349b7b3b5a4eb42c78182
Kris Hemenway <krishemenway@gmail.com>
2020-03-03 23:50:43 -0800

    moved GameTracker folder to GameTracker.Service to distinguish it from sibling folders better

---
+++ GameTracker.Service/GameProfiles/GameProfile.cs
@@ -0,0 +1,12 @@
+﻿using GameTracker.UserActivities;
+using System;
+using System.Collections.Generic;
+
+namespace GameTracker.GameProfiles
+{
+	public class GameProfile
+	{
+		public IReadOnlyList<IUserActivity> RecentActivity { get; set; }
+		public DateTimeOffset LastPlayedTime { get; set; }
+	}
+}
---
+++ GameTracker.Service/UserActivities/UserProfile.cs
@@ -0,0 +1,17 @@
+﻿using GameTracker.GameProfiles;
+using GameTracker.Games;
+using StronglyTyped.GuidIds;
+using System;
+using System.Collections.Generic;
+
+namespace GameTracker.UserActivities
+{
+	public class UserProfile
+	{
+		public string Name { get; set; }
+		public DateTimeOffset? StartedCollectingDataTime { get; set; }
+		public DateTimeOffset? MostRecentActivityTime { get; set; }
+		public int TotalGamesPlayed { get; set; }
+		public IReadOnlyDictionary<Id<Game>, GameProfile> GameProfilesByGameId { get; set; }
+	}
+}
