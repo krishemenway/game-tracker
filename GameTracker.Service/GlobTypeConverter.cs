b41942c3b7d7d9e4ff3ec8c2d68339ce5dde46a4
Kris Hemenway <krishemenway@gmail.com>
2020-03-22 16:36:41 -0700

    updated CSVHelper and Glob

--- GameTracker.Service/GlobTypeConverter.cs
+++ GameTracker.Service/GlobTypeConverter.cs
@@ -26,5 +26,32 @@ namespace GameTracker
 
 			return base.ConvertFrom(context, culture, value);
 		}
+
+		public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+		{
+			if (destinationType == typeof(string))
+			{
+				return true;
+			}
+
+			return base.CanConvertTo(context, destinationType);
+		}
+
+		public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+		{
+			if (value is Glob valueAsGlob)
+			{
+				if (destinationType == typeof(string))
+				{
+					return valueAsGlob.ToString();
+				}
+			}
+			else
+			{
+				throw new InvalidOperationException("Expected source type to be Glob");
+			}
+
+			return base.ConvertTo(context, culture, value, destinationType);
+		}
 	}
 }
