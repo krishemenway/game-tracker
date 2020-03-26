d812be8113818c843f76374a907edba5f236c993
Kris Hemenway <krishemenway@gmail.com>
2020-03-25 23:00:56 -0700

    removed static assets addition and added controller that points to app.html and app.js directly to help deal with routing in web

---
+++ GameTracker.Service/WebAssetsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System.IO;
+using System.Text;
+
+namespace GameTracker
+{
+	public class WebAssetsController : ControllerBase
+	{
+		public WebAssetsController(IMemoryCache memoryCache)
+		{
+			_memoryCache = memoryCache;
+		}
+
+		[HttpGet("app.js")]
+		public ContentResult AppJavascript()
+		{
+			return Content(ReadFileContents("app.js"), "application/javascript", Encoding.UTF8);
+		}
+
+		[HttpGet("")]
+		public ContentResult AppMarkup()
+		{
+			return Content(ReadFileContents("app.html"), "text/html", Encoding.UTF8);
+		}
+
+		private string ReadFileContents(string filePath)
+		{
+			return _memoryCache.GetOrCreate($"AssetsContents-{filePath}", (cache) => System.IO.File.ReadAllText(Path.Combine(Program.FilePathInAppData(filePath))));
+		}
+
+		private readonly IMemoryCache _memoryCache;
+	}
+}
