bc76358b505a91d38ea655d6caa6e66f550e9fbf
Kris Hemenway <krishemenway@gmail.com>
2020-05-08 02:18:21 -0700

    updating useObservable to use useLayoutEffect

--- GameTracker.Web/Common/useObservable.ts
+++ GameTracker.Web/Common/useObservable.ts
@@ -1,31 +1,8 @@
 import * as React from "react";
 import { ReadOnlyObservable } from "@residualeffect/reactor";
 
-let observableQueue: React.Dispatch<React.SetStateAction<{}>>[] = [];
-let timeout: number|null = null;
-
-function runQueue() {
-	for (let i = 0; i < observableQueue.length; ++i) {
-		observableQueue[i]({});
-	}
-
-	observableQueue = [];
-}
-
 export function useObservable<T>(observable: ReadOnlyObservable<T>): T {
 	const [, triggerReact] = React.useState({});
-
-	React.useEffect(() => {
-		return observable.Subscribe(() => {
-			observableQueue.push(triggerReact);
-
-			if (timeout !== null) {
-				clearTimeout(timeout);
-			}
-
-			timeout = setTimeout(runQueue, 0) as unknown as number;
-		});
-	}, [observable]);
-
+	React.useLayoutEffect(() => observable.Subscribe((newValue) => { triggerReact(newValue); }), [observable]);
 	return observable.Value;
 }
