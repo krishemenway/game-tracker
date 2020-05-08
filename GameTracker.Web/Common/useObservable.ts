48074ac8f7b15f2d271d4ce3929b59bcfa4e119c
Kris Hemenway <krishemenway@gmail.com>
2020-05-07 23:34:24 -0700

    upgrading useObservable

--- GameTracker.Web/Common/useObservable.ts
+++ GameTracker.Web/Common/useObservable.ts
@@ -1,8 +1,31 @@
-import { useEffect, useState } from "react";
+import * as React from "react";
 import { ReadOnlyObservable } from "@residualeffect/reactor";
- 
+
+let observableQueue: React.Dispatch<React.SetStateAction<{}>>[] = [];
+let timeout: number|null = null;
+
+function runQueue() {
+	for (let i = 0; i < observableQueue.length; ++i) {
+		observableQueue[i]({});
+	}
+
+	observableQueue = [];
+}
+
 export function useObservable<T>(observable: ReadOnlyObservable<T>): T {
-	const [, triggerReact] = useState({});
-	useEffect(() => { return observable.Subscribe(() => triggerReact({})); }, [observable]);
+	const [, triggerReact] = React.useState({});
+
+	React.useEffect(() => {
+		return observable.Subscribe(() => {
+			observableQueue.push(triggerReact);
+
+			if (timeout !== null) {
+				clearTimeout(timeout);
+			}
+
+			timeout = setTimeout(runQueue, 0) as unknown as number;
+		});
+	}, [observable]);
+
 	return observable.Value;
 }
