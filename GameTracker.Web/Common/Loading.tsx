47386fe66ea0954539739526870ae8c25523044d
Kris Hemenway <krishemenway@gmail.com>
2020-05-07 18:32:21 -0700

    tree-shaking is not working with @material-ui, so a more specific reference is needed

--- GameTracker.Web/Common/Loading.tsx
+++ GameTracker.Web/Common/Loading.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { CircularProgress } from "@material-ui/core";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import { ObservableLoading } from "Common/ObservableLoading";
 import { useObservable } from "Common/useObservable";
 
