1efde10a783ed6d1d6a0683f9d06c447bb6d987e
Kris Hemenway <krishemenway@gmail.com>
2020-05-11 19:22:44 -0700

    wrapping UserActivity sets in class to include statistics

---
+++ GameTracker.Service/UserActivities/UserActivityForDate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace GameTracker.UserActivities
+{
+	public class UserActivityForDate
+	{
+		public UserActivityForDate(IReadOnlyList<IUserActivity> userActivities)
+		{
+			AllUserActivity = userActivities;
+		}
+
+		public IReadOnlyList<IUserActivity> AllUserActivity { get; }
+
+		public int TotalActivityCount => AllUserActivity.Count;
+		public double TotalTimeSpentInSeconds => AllUserActivity.Sum(x => x.TimeSpentInSeconds);
+	}
+}
