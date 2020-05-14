851b034a48c0bd616999d5d0e842c661cc2c0459
Kris Hemenway <krishemenway@gmail.com>
2020-05-13 23:47:55 -0700

    removing public setter on HasLoaded in Loadable

--- GameTracker.Web/Common/Loadable.ts
+++ GameTracker.Web/Common/Loadable.ts
@@ -11,8 +11,7 @@ export class Loadable<TSuccessData> implements LoadableBase {
 		this._isLoading = new Observable(false);
 		this._errorMessage = new Observable(null);
 		this._successData = new Observable(null);
-
-		this.HasLoaded = new Computed(() => this.SuccessData.Value !== null);
+		this._hasLoaded = new Computed(() => this.SuccessData.Value !== null);
 	}
 
 	public StartLoading(): Loadable<TSuccessData> {
@@ -28,7 +27,7 @@ export class Loadable<TSuccessData> implements LoadableBase {
 		this._isLoading.Value = false;
 	}
 
-	public FailedLoading(errorMessage: string) {
+	public FailedLoading(errorMessage: string): void {
 		this._successData.Value = null;
 		this._errorMessage.Value = errorMessage;
 		this._isLoading.Value = false;
@@ -37,10 +36,10 @@ export class Loadable<TSuccessData> implements LoadableBase {
 	public get IsLoading(): ReadOnlyObservable<boolean> { return this._isLoading; }
 	public get ErrorMessage(): ReadOnlyObservable<string|null>  { return this._errorMessage; }
 	public get SuccessData(): ReadOnlyObservable<TSuccessData|null>  { return this._successData; }
+	public get HasLoaded(): ReadOnlyObservable<boolean> { return this._hasLoaded; }
 
 	private _isLoading: Observable<boolean>;
 	private _errorMessage: Observable<string|null>;
 	private _successData: Observable<TSuccessData|null>;
-
-	public HasLoaded: Computed<boolean>;
+	private _hasLoaded: Computed<boolean>;
 }
