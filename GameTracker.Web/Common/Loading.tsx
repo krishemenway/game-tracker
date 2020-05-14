d857d62e14977ac993d43001e23ba20b2dc6e02c
Kris Hemenway <krishemenway@gmail.com>
2020-05-13 23:33:16 -0700

    using unknown over any for accuracy

--- GameTracker.Web/Common/Loading.tsx
+++ GameTracker.Web/Common/Loading.tsx
@@ -10,11 +10,11 @@ function Loading<A, B>(props: { loadables: [Loadable<A>, Loadable<B>], renderSuc
 function Loading<A, B, C>(props: { loadables: [Loadable<A>, Loadable<B>, Loadable<C>], renderSuccess: (a: A, b: B, c: C) => JSX.Element }): JSX.Element;
 function Loading<A, B, C, D>(props: { loadables: [Loadable<A>, Loadable<B>, Loadable<C>, Loadable<D>], renderSuccess: (a: A, b: B, c: C, d: D) => JSX.Element }): JSX.Element;
 
-function Loading(props: { loadables: Loadable<any>[], renderSuccess: (...inputValues: any[]) => JSX.Element }): JSX.Element {
+function Loading(props: { loadables: Loadable<unknown>[], renderSuccess: (...inputValues: unknown[]) => JSX.Element }): JSX.Element {
 	const text = useTextStyles();
 	const layout = useLayoutStyles();
 
-	let successDatas: any[] = [];
+	let successDatas: unknown[] = [];
 	let hasLoaded = true;
 	let errorMessage: string|null = null;
 
