d00986055de49825b48c87b10d7ef55a5574747e
Kris Hemenway <krishemenway@gmail.com>
2020-05-30 00:36:33 -0700

    added UserActivityForMonth endpoint

---
+++ GameTracker.Service/UserActivities/UserActivityForMonthController.cs
@@ -0,0 +1,63 @@
+﻿using GameTracker.Games;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GameTracker.UserActivities
+{
+	[Route("WebAPI")]
+	public class UserActivityForMonthController : ControllerBase
+	{
+		public UserActivityForMonthController(
+			IMemoryCache memoryCache,
+			AllUserActivityCache allUserActivityCache = null,
+			IGameStore gameStore = null)
+		{
+			_allUserActivityCache = allUserActivityCache ?? new AllUserActivityCache(memoryCache);
+			_gameStore = gameStore ?? new GameStore();
+		}
+
+		[HttpGet(nameof(UserActivityForMonth))]
+		public ActionResult<UserActivityForMonthResponse> UserActivityForMonth([FromQuery] int month, [FromQuery] int year)
+		{
+			var firstOfMonth = DateTimeOffset.Parse($"{year}-{month}-01");
+			var lastOfMonth = firstOfMonth.AddMonths(1).Subtract(TimeSpan.FromSeconds(1));
+
+			var userActivityForMonth = _allUserActivityCache
+				.FindUserActivity(firstOfMonth, lastOfMonth)
+				.OrderBy(x => x.AssignedToDate)
+				.ThenBy(x => x.EndTime).ToList();
+
+			var distinctGameIds = userActivityForMonth
+				.Select(x => x.GameId)
+				.Distinct().ToList();
+
+			return new UserActivityForMonthResponse
+			{
+				AllUserActivity = userActivityForMonth,
+
+				TimeSpentInSecondsByGameId = userActivityForMonth.GroupBy(x => x.GameId).ToDictionary(x => x.Key.Value, x => x.Sum(y => y.TimeSpentInSeconds)),
+				TimeSpentInSecondsByDate = userActivityForMonth.GroupBy(x => x.AssignedToDate).ToDictionary(x => x.Key.ToString("yyyy-MM-dd"), x => x.Sum(y => y.TimeSpentInSeconds)),
+				GamesByGameId = _gameStore.FindGames(distinctGameIds).ToDictionary(x => x.Key.Value, x => x.Value),
+
+				TotalGamesPlayed = distinctGameIds.Count,
+				TotalTimePlayedInSeconds = userActivityForMonth.Sum(x => x.TimeSpentInSeconds),
+			};
+		}
+
+		private readonly AllUserActivityCache _allUserActivityCache;
+		private readonly IGameStore _gameStore;
+	}
+
+	public class UserActivityForMonthResponse
+	{
+		public IReadOnlyList<UserActivity> AllUserActivity { get; set; }
+		public Dictionary<string, double> TimeSpentInSecondsByGameId { get; set; }
+		public Dictionary<string, double> TimeSpentInSecondsByDate { get; set; }
+		public Dictionary<string, IGame> GamesByGameId { get; set; }
+		public int TotalGamesPlayed { get; set; }
+		public double TotalTimePlayedInSeconds { get; set; }
+}
+}
