da4dd219a0b571528460c762ae2717a97b8a1bae
Kris Hemenway <krishemenway@gmail.com>
2020-05-30 23:16:24 -0700

    added user activity for month into MonthView

--- GameTracker.Web/UserActivities/MonthView.tsx
+++ GameTracker.Web/UserActivities/MonthView.tsx
@@ -7,23 +7,29 @@ import { UserProfileService } from "UserProfile/UserProfileService";
 import StatisticsSection from "Common/StatisticsSection";
 import PageHeader from "Common/PageHeader";
 import PageFooter from "Common/PageFooter";
+import { UserActivityService } from "UserActivities/UserActivityService";
+import { UserActivityForMonthResponse } from "UserActivities/UserActivityForMonthResponse";
+import { TimeSpan } from "Common/TimeSpan";
 
-const MonthView: React.FC<{ year:string; month: string; className?: string }> = (props) => {
+const MonthView: React.FC<{ year: string; month: string; className?: string }> = (props) => {
 	const layout = useLayoutStyles();
+	const year = parseInt(props.year, 10);
+	const month = parseInt(props.month, 10);
 
+	React.useEffect(() => { UserActivityService.Instance.LoadForMonth(year, month); }, []);
 	React.useEffect(() => { UserProfileService.Instance.LoadProfile(); }, []);
 
 	return (
 		<div className={layout.centerLayout1000}>
 			<Loading
-				loadables={[UserProfileService.Instance.LoadingUserProfile]}
-				renderSuccess={(userProfile) => <LoadedMonthView monthKey={`${props.year}-${props.month}`} userName={userProfile.UserName} />}
+				loadables={[UserProfileService.Instance.LoadingUserProfile, UserActivityService.Instance.FindOrCreateUserActivityForMonth(`${year}-${month}`)]}
+				renderSuccess={(userProfile, userActivityForMonth) => <LoadedMonthView monthKey={`${props.year}-${props.month}`} userName={userProfile.UserName} userActivityForMonth={userActivityForMonth} />}
 			/>
 		</div>
 	);
 };
 
-const LoadedMonthView: React.FC<{ monthKey: string; userName: string }> = (props) => {
+const LoadedMonthView: React.FC<{ monthKey: string; userName: string; userActivityForMonth: UserActivityForMonthResponse }> = (props) => {
 	const layout = useLayoutStyles();
 	const dateAsMoment = React.useMemo(() => moment(props.monthKey + "-01"), [props.monthKey]);
 
@@ -34,13 +40,14 @@ const LoadedMonthView: React.FC<{ monthKey: string; userName: string }> = (props
 			<div className={clsx(layout.flexRow, layout.flexEvenDistribution, layout.flexItemSpacing)}>
 				<StatisticsSection
 					statistics={[
-						{ Label: "Statistics", Value: "Coming Soon" },
+						{ Label: "Total Games Played", Value: props.userActivityForMonth.TotalGamesPlayed },
+						{ Label: "Total Sessions", Value: props.userActivityForMonth.AllUserActivity.length },
 					]}
 				/>
 
 				<StatisticsSection
 					statistics={[
-						{ Label: "Statistics", Value: "Coming Soon" },
+						{ Label: "Total Time Played", Value: TimeSpan.Readable(props.userActivityForMonth.TotalTimePlayedInSeconds) },
 					]}
 				/>
 			</div>
