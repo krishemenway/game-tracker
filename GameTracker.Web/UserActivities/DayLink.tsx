7c39c0f386ec02ee41de5f949625880d60cb7724
Kris Hemenway <krishemenway@gmail.com>
2020-07-09 01:52:41 -0700

    suppressing DayLink when date is in the future

--- GameTracker.Web/UserActivities/DayLink.tsx
+++ GameTracker.Web/UserActivities/DayLink.tsx
@@ -4,14 +4,15 @@ import clsx from "clsx";
 import { Link, useLocation } from "react-router-dom";
 import { useActionStyles } from "AppStyles";
 
-const DayLinkOrLabel: React.FC<{ date: moment.Moment, className?: string }> = (props) => {
+const currentDate = moment();
+const DayLinkOrLabel: React.FC<{ date: moment.Moment }> = (props) => {
 	const location = useLocation();
 	const action = useActionStyles();
 
 	const isViewingDay = location.pathname.match(/^\/activity\/([^\/]+)\/([^\/]+)\/([^\/]+)$/i) !== null;
-	return isViewingDay
-		? <span className={props.className}>{props.children}</span>
-		: <Link className={clsx(action.clickable, action.clickableUnderline, props.className)} to={`/activity/${props.date.format("YYYY/MM/DD")}`}>{props.children}</Link>;
+	return isViewingDay || props.date.isAfter(currentDate)
+		? <>{props.children}</>
+		: <Link className={clsx(action.clickable, action.clickableUnderline)} to={`/activity/${props.date.format("YYYY/MM/DD")}`}>{props.children}</Link>;
 };
 
 export default DayLinkOrLabel;
