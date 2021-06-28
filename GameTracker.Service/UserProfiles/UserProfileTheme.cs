3b55c421f51f12bd25a0beab295499940574342a
Kris Hemenway <krishemenway@gmail.com>
2021-06-27 22:14:24 -0700

    adding alternating panel background color and panel border color to themes

--- GameTracker.Service/UserProfiles/UserProfileTheme.cs
+++ GameTracker.Service/UserProfiles/UserProfileTheme.cs
@@ -3,8 +3,9 @@
 	public class UserProfileTheme
 	{
 		public string PanelBackgroundColor { get; set; }
+		public string PanelAlternatingBackgroundColor { get; set; }
+		public string PanelBorderColor { get; set; }
 		public string PageBackgroundColor { get; set; }
-
 		public string PrimaryTextColor { get; set; }
 		public string SecondaryTextColor { get; set; }
 	}
