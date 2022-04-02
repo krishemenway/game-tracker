849c61a17bc6d3a9c4eaef87ef2ec3077bd60d67
Kris Hemenway <krishemenway@gmail.com>
2022-04-02 16:37:04 -0700

    added endpoint for finding observed processes using globs for testing File Match Patterns

---
+++ GameTracker.Service/ControlPanel/ObservedProcessesController.cs
@@ -0,0 +1,32 @@
+﻿using GameTracker.ObservedProcesses;
+using GlobExpressions;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GameTracker.ControlPanel
+{
+	[Route("WebAPI/ControlPanel")]
+	[RestrictToLocalhost]
+	public class ObservedProcessesController : ControllerBase
+	{
+		[HttpPost(nameof(FindObservedProcesses))]
+		public ActionResult<FindObservedProcessesResponse> FindObservedProcesses([FromQuery] string searchQuery)
+		{
+			var searchGlob = new Glob(searchQuery, GlobOptions.CaseInsensitive);
+
+			return new FindObservedProcessesResponse
+				{
+					Processes = new ObservedProcessStore().FindAll()
+						.Where(p => searchGlob.IsMatch(p.ProcessPath))
+						.OrderByDescending(p => p.FirstObservedTime)
+						.ToArray(),
+				};
+		}
+	}
+
+	public class FindObservedProcessesResponse
+	{
+		public IReadOnlyList<ObservedProcess> Processes { get; set; }
+	}
+}
